7af00e0a31daf09279239262b3c836316da65d14
Robert Long <robmlong@gmail.com>
2017-08-31 21:13:17 +0100

    x -> y

--- src/my_square.py
+++ src/my_square.py
@@ -1,7 +1,7 @@
-def my_square(x):
+def my_square(y):
 		"""takes a value and returns the square value
 		
 		uses the ** operator"""
-		return(x ** 2)
+		return(y ** 2)
 
-print(my_square(4))
+print(my_square(42))
